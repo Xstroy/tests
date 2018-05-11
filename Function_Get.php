200d12f4cdfed85c014f128cbfa50f3da529adf6
Xstroy <39198449+Xstroy@users.noreply.github.com>
2018-05-11 21:06:57 +0300

    Update Function_Get.php

--- Function_Get.php
+++ Function_Get.php
@@ -1,20 +1,7 @@
 
 
 <?php
-$username='lidavzn';
-echo "<pre>";
-/*
-print_r(getPublicInfo($username));
 
-function getPublicInfo($username) {
-    $url     = sprintf("https://www.instagram.com/$username");
-    $content = file_get_contents($url);
-    $content = explode("window._sharedData = ", $content)[1];
-    $content = explode(";</script>", $content)[0];
-    $data    = json_decode($content, true);
-    return $data['entry_data']['ProfilePage'][0];
-}
-*/
 $hashtag="москва";
 print_r(getPublicHashtag($hashtag));
 function getPublicHashtag($hashtag) {
